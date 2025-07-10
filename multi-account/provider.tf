3df4df695e70945c6c1f8cac13cb966cee945790
subbu <subbunandyala48@gmail.com>
2025-07-10 18:10:25 +0530

    terraform

---
+++ multi-account/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+    required_providers {
+        aws  = {
+            source ="hashicorp/aws" 
+            version = "5.98.0"
+        }
+    }
+}
+
+provider "aws" {
+    #configuration options 
+    alias  = "dev" 
+    profile  = "dev"
+}
+
+provider "aws" {
+    #configuration options 
+    alias = "prod"
+    profile  = "prod"
+}
