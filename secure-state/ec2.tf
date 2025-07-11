f4d3b010e36c616d3a9588b63c5f68330148bf38
subbu <subbunandyala48@gmail.com>
2025-07-11 18:01:22 +0530

    terraform

--- secure-state/ec2.tf
+++ secure-state/ec2.tf
@@ -9,7 +9,7 @@ resource "aws_instance" "roboshop" {
 }
 
 resource "aws_security_group" "allow_all" {
-    name = "allow_all"
+    name = "allow_all_change"
     description = "allow all traffic"
 
     ingress {
