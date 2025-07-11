c29dbbcb0d90c1f95ef648c1a331c2fb35bf8de1
subbu <subbunandyala48@gmail.com>
2025-07-11 17:59:11 +0530

    terraform

--- secure-state/ec2.tf
+++ secure-state/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "roboshop" {
-    ami = ""
+    ami = "ami-09c813fb71547fc4f"
     instance_type = "t3.micro"
     vpc_security_group_ids = ["aws_security_group.allow_all.id"]
 
