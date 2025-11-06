1a186edc08d1198918d5af0b42fba5a4a00582d6
subbu <subbunandyala48@gmail.com>
2025-11-06 16:45:09 +0530

    terraform

--- secure-state/ec2.tf
+++ secure-state/ec2.tf
@@ -20,7 +20,7 @@ resource "aws_security_group" "allow_all_change" {
         ipv6_cidr_blocks = ["::/0"]
     }
 
-    egress {
+    egress { 
         from_port = 0 
         to_port   = 0
         protocol  = "-1" 
--- secure-state/provider.tf
+++ secure-state/provider.tf
@@ -2,14 +2,14 @@ terraform {
     required_providers {
         aws = {
             source = "hashicorp/aws" 
-            version  = "5.98.0"
+            version = "5.98.0"
         }
     }
     backend "s3" {
-        bucket  = "secure-state-tests"
-        key     = "secure" 
-        region  = "us-east-1" 
-        encrypt  = true 
+        bucket = "90-secure-state-tests" 
+        key    = "secure" 
+        region = "us-east-1" 
+        encrypt = true 
         use_lockfile = true 
     }
 }
