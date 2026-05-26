80c6ae9db72c29099a3cdedd95dab1bb99229889
subbu <subbunandyala48@gmail.com>
2026-05-26 16:29:03 +0530

    terraform

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -20,7 +20,7 @@ resource "aws_security_group" "allow_all" {
         protocol  = "-1" 
         cidr_blocks = ["0.0.0.0/0"] 
         ipv6_cidr_blocks = ["::/0"]
-    }
+    } 
 
     egress {
         from_port = "0" 
