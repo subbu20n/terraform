89a9ae418920b9c86993c8f2898229abac4586ce
subbu <subbunandyala48@gmail.com>
2026-06-25 20:15:20 +0530

    terraform

--- conditions/ec2.tf
+++ conditions/ec2.tf
@@ -24,7 +24,7 @@ resource "aws_security_group" "allow_all" {
         to_port   = var.to_port  
         protocol  = "-1" 
         cidr_blocks = var.cidr_blocks 
-        ipv6_cidr_blocks = ["::/0"]
+        ipv6_cidr_blocks = ["::/0"]   
     }
 
     tags = var.sg_tags 
