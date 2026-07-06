88926bba170ea954b4ef4508a49339ab1d08367d
subbu <subbunandyala48@gmail.com>
2026-07-06 23:36:52 +0530

    conditions

--- conditions/ec2.tf
+++ conditions/ec2.tf
@@ -12,14 +12,14 @@ resource "aws_security_group" "allow_all" {
     description = var.sg_description 
 
     ingress {
-        from_port = var.from_port 
+        from_port = var.from_port   
         to_port = var.to_port 
         protocol = "-1" 
         cidr_blocks = var.cidr_blocks  
         ipv6_cidr_blocks = ["::/0"]
     } 
   
-    egress {  
+    egress {    
         from_port = var.from_port 
         to_port   = var.to_port  
         protocol  = "-1" 
