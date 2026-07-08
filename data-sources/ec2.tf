306f07e66b3f59a25f9833d6c5a81cce5e7ad8f8
subbu <subbunandyala48@gmail.com>
2026-07-09 00:11:46 +0530

    terraform

--- data-sources/ec2.tf
+++ data-sources/ec2.tf
@@ -17,7 +17,7 @@ resource "aws_security_group" "allow_all" {
     ingress { 
         from_port = "0"  
         to_port   = "0" 
-        protocol  = "-1" 
+        protocol  = "-1"     
         cidr_blocks = ["0.0.0.0/0"]  
         ipv6_cidr_blocks = ["::/0"]
     }
